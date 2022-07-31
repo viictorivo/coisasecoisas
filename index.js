9baef546ff2ab37164a18b8802d814f151f1baab
Your Name <you@example.com>
2022-07-31 11:39:50 -0300

    ajustando bd

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ connection
     console.log(err);
 })
 
+app.post("/saveproduct", (req, res) => {
+    var nome = req.body.nome;
+    var tipo = req.body.tipo;
+    var preco_venda = req.body.preco_venda;
+    var preco_custo = req.body.preco_custo;
+    var quantidade = req.body.quantidade;
+    var data_compra = req.body.data_compra;
+});
+
+app.get("/listproducts", (req, res) =>{
+
+});
+
+app.patch("/updateproduct", (req, res) => {
+
+})
+
+app.delete("/product", (req, res) => {
+    
+})
+
 app.listen( 8080, () => {
     console.log("api rodando");
 })
